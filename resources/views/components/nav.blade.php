cf1b372bea2c72abb04a8cc37df1a5d53f622388
PSL2001 <65461723+PSL2001@users.noreply.github.com>
2021-06-04 23:29:51 +0200

    Creado las plantillas, empezamos por perfiles

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid mx-2">
+      <a class="navbar-brand" href="#">Foromotos S.A</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active mx-2" aria-current="page" href="/"><i class="fas fa-home"></i> Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-2" href="{{route('usuarios.index')}}"><i class="far fa-address-card"></i> Gestionar Usuarios</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link mx-2" href="{{route('perfils.index')}}"><i class="fas fa-book-reader"></i> Gestionar Perfiles</a>
+            </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
